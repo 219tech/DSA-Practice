a071d78f92928e2c95d8a606bc0163e74b29cce1
219tech <28ankitsingh@gmail.com>
2021-12-28 19:14:26 +0530

    Create TwoSum.java
    TwoSum.java

---
+++ arrays/TwoSum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for(int i=0; i<nums.length; i++){
+                if(map.get(nums[i]) != null){
+                    result[0] = map.get(nums[i]);
+                    result[1] = i;
+                    break;
+                }
+                map.put(target - nums[i] , i);
+        }
+        return result;
+    }
+}
